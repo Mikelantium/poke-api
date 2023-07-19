2cfa4b55ff5224ddfeebd2809d237ec1090b9f52
Mikelantium <leugimmail@gmail.com>
2023-07-19 23:51:14 +0200

    Actualización de la pokédex

--- script/pokedex.js
+++ script/pokedex.js
@@ -1,27 +1,37 @@
-// Constantes globales
+// Constantes globales.
 const pokedex = document.getElementById("pokedex");
 const searchInput = document.getElementById("searchInput");
-let allPokemonData = []; // Variable para almacenar los datos de todos los Pokémon
+let allPokemonData = []; // Variable para almacenar los datos de todos los Pokémon.
 
-// Función para obtener los datos de todos los Pokémon
+// Función para obtener los datos de los Pokémon.
 const fetchPokemon = async () => {
-    for (let i = 1; i <= 150; i++) {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-      const data = await response.json();
-      const pokemon = {
-        name: data.name,
-        image: data.sprites.versions["generation-v"]["black-white"].animated
-        .front_default,
-        type: data.types.map((type) => type.type.name).join(", "),
-        id: data.id,
-      };
-      allPokemonData.push(pokemon); // Almacenar los datos del Pokémon en el arreglo
-    }
-    displayPokemonList(allPokemonData); // Mostrar todos los Pokémon en la lista inicial
-  } 
+  const pokemons = [];
+  for (let i = 1; i <= 150; i++) {
+    const pokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    const pokemonsJson = await pokemonsResponse.json();
+    pokemons.push(pokemonsJson);
+  }
+  return pokemons;
+};
 
+const initializeApp = async () => {
+  
+    const pokemons = await fetchPokemon();
+    allPokemonData = mappedPokemons(pokemons);
+    displayPokemonList(allPokemonData); // Mostrar todos los Pokémon en la lista inicial.
+};
 
-// Función para mostrar un Pokémon en la lista
+const mappedPokemons = (pokemons) => {
+  const mapPokemons = pokemons.map((data) => ({
+    name: data.name,
+    image: data.sprites.versions["generation-v"]["black-white"].animated.front_default,
+    type: data.types.map((type) => type.type.name).join(", "),
+    id: data.id,
+  }));
+  return mapPokemons;
+};
+
+// Función para mostrar un Pokémon en la lista.
 const displayPokemon = (pokemon) => {
   const pokemonElement = document.createElement("li");
   pokemonElement.classList.add("card");
@@ -34,22 +44,21 @@ const displayPokemon = (pokemon) => {
   pokedex.appendChild(pokemonElement);
 };
 
-// Función para mostrar una lista de Pokémon
+// Función para mostrar una lista de Pokémon.
 const displayPokemonList = (pokemonList) => {
-  pokedex.innerHTML = ""; // Limpiar la lista de Pokémon antes de mostrar nuevos resultados
+  pokedex.innerHTML = ""; // Limpiar la lista de Pokémon antes de mostrar nuevos resultados.
   pokemonList.forEach((pokemon) => {
-    displayPokemon(pokemon); // Mostrar cada Pokémon en la lista
+    displayPokemon(pokemon); // Mostrar cada Pokémon en la lista.
   });
 };
 
-// Escuchador de eventos para la búsqueda de Pokémon
+// Escuchador de eventos para la búsqueda de Pokémon.
 searchInput.addEventListener("input", () => {
   const searchTerm = searchInput.value.toLowerCase();
   const filteredPokemon = allPokemonData.filter((pokemon) =>
     pokemon.name.includes(searchTerm)
   );
-  displayPokemonList(filteredPokemon); // Mostrar solo los Pokémon filtrados según el término de búsqueda
+  displayPokemonList(filteredPokemon); 
 });
 
-fetchPokemon();
- 
+initializeApp();
